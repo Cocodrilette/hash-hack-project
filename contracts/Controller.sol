4a4d0f46e4f2a9b4b56e90005982709e0750493a
tomasCalletce <64665732+tomasCalletce@users.noreply.github.com>
2023-07-21 20:57:11 -0500

    setup

---
+++ contracts/Controller.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Controller is Ownable {
+
+    event makeCommit(bytes32 indexed hash, uint indexed blockNumber);
+    event makeReveal(bytes32 indexed hash, uint indexed blockNumber);
+
+    struct Commitment {
+        bytes32 hash;
+        bool complete;
+    }
+
+    mapping(bytes32 => Commitment) public commitments;
+
+    function commit(bytes32 hashOfOrder) external onlyOwner {
+        Commitment memory com = Commitment({
+            hash : hashOfOrder,
+            complete : false
+        });
+        
+        commitments[hashOfOrder] = com;
+
+        emit makeCommit(hashOfOrder,block.number);
+    }
+
+    function reveal(
+        string memory tickerSymbol,
+        string memory orderType,
+        string memory accountType,
+        uint quantity,
+        uint price,
+        uint timeInForce,
+        uint direction
+    ) external onlyOwner {
+        bytes32 hash = keccak256(abi.encode(
+            tickerSymbol,
+            orderType,
+            accountType,
+            quantity,
+            price,
+            timeInForce,
+            direction
+        ));
+        require(hash == commitments[hash].hash);
+
+        commitments[hash].complete = true;
+
+        emit makeReveal(hash,block.number);
+    }
+    
+}
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,13 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    hardhat: {},
+    avalanche: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+  },
 };
 
 export default config;

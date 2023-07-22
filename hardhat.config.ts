3f059dff05d7ce723b4bb95e692c25621bb793fa
tomasCalletce <64665732+tomasCalletce@users.noreply.github.com>
2023-07-21 21:06:01 -0500

    done

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,13 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+require('dotenv').config();
+
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
   networks: {
     hardhat: {},
-    avalanche: {
+    fuji: {
       url: "https://api.avax-test.network/ext/bc/C/rpc",
       accounts: [`0x${process.env.PRIVATE_KEY}`],
     },
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,24 +1,15 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const con = await ethers.deployContract("Controller");
+  await con.waitForDeployment();
 
-  const lockedAmount = ethers.parseEther("0.001");
-
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log('Address: ' + await con.getAddress());
 }
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+
+// contract 0x754B75A0c622bB5f3Da5068636596aC0F2ca2e73
